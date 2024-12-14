f8fdfc0bc64f7c5515d3d5c6c01fba1dab24b2fc
Raphael Carubbi <rcarubbi@gmail.com>
2024-12-14 01:32:07 +0000

    Initial Commit

---
+++ PdfFromRazor/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+using PdfFromRazor.Invoices;
+
+// run once to install the browsers
+//Microsoft.Playwright.Program.Main(["install"]);
+//return;
+
+IServiceCollection services = new ServiceCollection();
+services.AddLogging();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+
+var invoice = InvoiceGenerator.Generate();
+
+var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+{
+
+    var dictionary = new Dictionary<string, object?>
+    {
+        { "Invoice", invoice },
+    };
+
+    var parameters = ParameterView.FromDictionary(dictionary);
+    var output = await htmlRenderer.RenderComponentAsync<InvoiceView>(parameters);
+
+    return output.ToHtmlString();
+});
+
+
+using var playwright = await Playwright.CreateAsync();
+
+var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+{
+    Headless = true
+});
+
+var page = await browser.NewPageAsync();
+await page.SetContentAsync(html);
+
+await page.PdfAsync(new PagePdfOptions
+{
+    Format = "A4",
+    Path = "./invoice.pdf"
+});
+
+await page.CloseAsync();
+
+
+
+
+
